de6d76d189eaa11796faaf716efab79621852b9f
ianchb <admin@cgtsoft.com>
2022-01-03 14:03:19 +0800

    Delete 2.1.x directory

--- 2.1.x/post-fs-data.sh
+++
@@ -1,8 +0,0 @@
-MODDIR=${0%/*}
-TARGET_PATH="/product/etc/device_features"
-CHANGE=$MODDIR/system/$TARGET_PATH
-mkdir -p $CHANGE
-rm $CHANGE/*
-cp -f $TARGET_PATH/* $CHANGE/
-sed -i 's/"support_ota_validate">true</"support_ota_validate">false</g' $CHANGE/*.xml
-ui_print "**Key support_ota_validate changed successfully.**"
