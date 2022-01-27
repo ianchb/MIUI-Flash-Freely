b5859f8ddacf58dc1acc1003c1965c61ede28fa9
ianchb <admin@cgtsoft.com>
2022-01-27 17:36:58 +0800

    Add files via upload
    3.1.0127

---
+++ 3.1.0127/customize.sh
@@ -0,0 +1,21 @@
+ui_print "**************************"
+ui_print "MIUI Flash Freely v3.1.0127"
+ui_print "**************************"
+CHECK="/vendor/etc
+/product/etc"
+SUPPORT="0"
+for path in $CHECK; do
+  if [[ -d $path/device_features ]]; then
+    ui_print "- Specific Directory Found!"
+    SUPPORT="1"
+    break
+  else
+    ui_print "! Warning: Directory $path/device_features Not Found."
+    ui_print "- Trying another one."
+  fi
+done
+if [[ $SUPPORT == "0" ]]; then
+  abort "! Error: Unsupported device!"
+else
+  ui_print "- Completed successfully!"
+fi
