4e9a5e6986007de519ecfa84735cd4d212a872fc
ianchb <admin@cgtsoft.com>
2023-02-18 22:53:47 +0800

    Solve conflicts with MIUI-AOD-Freely

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -11,5 +11,11 @@ done
 CHANGE=$MODDIR/system/$TARGET_PATH
 mkdir -p $CHANGE
 rm $CHANGE/*
-cp -f $TARGET_PATH/* $CHANGE/
+if [ ! -f $MODDIR/../maodf.marker ]; then
+    cp -f $TARGET_PATH/* $CHANGE/
+    touch $MODDIR/../mff.marker
+else
+    cp -f $MODDIR/../MIUI_AOD_Freely/system/$TARGET_PATH/* $CHANGE/
+    rm -f $MODDIR/../maodf.marker
+fi
 sed -i 's/"support_ota_validate">true</"support_ota_validate">false</g' $CHANGE/*.xml
